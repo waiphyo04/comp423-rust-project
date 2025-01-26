b0cf061ff7a175bf1058d6a2fc4232008fd2ce8e
waiphyo04 <danielwaiphyo25204@gmail.com>
2025-01-26 23:35:00 +0000

    Finished rust Hello World

---
+++ Hello-World/target/debug/Hello-World.d
@@ -0,0 +1,1 @@
+/workspaces/rust-language-tutorial/Hello-World/target/debug/Hello-World: /workspaces/rust-language-tutorial/Hello-World/src/main.rs
---
+++ Hello-World/target/debug/deps/Hello_World-23f10c1d580ea8e4.d
@@ -0,0 +1,5 @@
+/workspaces/rust-language-tutorial/Hello-World/target/debug/deps/Hello_World-23f10c1d580ea8e4: src/main.rs
+
+/workspaces/rust-language-tutorial/Hello-World/target/debug/deps/Hello_World-23f10c1d580ea8e4.d: src/main.rs
+
+src/main.rs:
